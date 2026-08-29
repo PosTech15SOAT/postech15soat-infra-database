85724ec5c3a705df8a35059f041cace1be95490b
AndersonMuniz <anderson_c_m@yahoo.com.br>
2026-08-29 17:48:18 -0300

    style: formata arquivos terraform

--- locals.tf
+++ locals.tf
@@ -1,8 +1,8 @@
 locals {
   common_tags = {
-    Project    = var.project_name
-    ManagedBy  = "Terraform"
-    Repository = "PosTech15SOAT-Infra-Banco"
+    Project     = var.project_name
+    ManagedBy   = "Terraform"
+    Repository  = "PosTech15SOAT-Infra-Banco"
     Environment = var.environment
   }
 }
--- modules/rds/main.tf
+++ modules/rds/main.tf
@@ -92,14 +92,14 @@ resource "aws_db_instance" "this" {
   vpc_security_group_ids = [aws_security_group.this.id]
   parameter_group_name   = aws_db_parameter_group.this.name
 
-  publicly_accessible         = false
-  multi_az                    = false
-  auto_minor_version_upgrade  = true
-  backup_retention_period     = var.backup_retention_period
-  deletion_protection         = var.deletion_protection
-  skip_final_snapshot         = true
-  copy_tags_to_snapshot       = true
-  apply_immediately           = true
+  publicly_accessible        = false
+  multi_az                   = false
+  auto_minor_version_upgrade = true
+  backup_retention_period    = var.backup_retention_period
+  deletion_protection        = var.deletion_protection
+  skip_final_snapshot        = true
+  copy_tags_to_snapshot      = true
+  apply_immediately          = true
 
   tags = {
     Name = "${var.project_name}-${var.environment}-postgres"
